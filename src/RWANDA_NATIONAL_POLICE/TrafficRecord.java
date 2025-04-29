8bda2a8d233419e0df8a1416dcc66267b2b38408
BYIRINGIRO Bailly <byiringirobailly@gmail.com>
2025-04-29 14:06:55 +0200

    TrafficRecord.java

--- src/RWANDA_NATIONAL_POLICE/TrafficRecord.java
+++ src/RWANDA_NATIONAL_POLICE/TrafficRecord.java
@@ -1,14 +1,14 @@
 package RWANDA_NATIONAL_POLICE;
 
 
-// Abstract class
+
 public abstract class TrafficRecord {
-    protected String driverId;        // National ID or driving license number
-    protected String driverName;      // Driver's full name
-    protected String vehiclePlate;     // Vehicle registration plate
-    protected String violationType;    // Type of violation
-    protected double fineAmount;       // Amount of the fine
-    protected String paymentStatus;    // Payment status ("UNPAID" or "PAID")
+    protected String driverId;       
+    protected String driverName;      
+    protected String vehiclePlate;     
+    protected String violationType;    
+    protected double fineAmount;       
+    protected String paymentStatus;    
 
     public abstract void recordViolation();
     public abstract void assessFine();
